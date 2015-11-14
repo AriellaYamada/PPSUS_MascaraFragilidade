354f99f302049d05115dac883f8407f4629fc17b
AriellaYamada <ariella.y.b@hotmail.com>
2015-11-13 23:11:02 -0200

    Aplicando condição em Q1

---
+++ Mascara/js/fragility.js
@@ -0,0 +1,12 @@
+function validate_c23a () {
+    document.getElementById("opt").innerHTML = '<div class="col-lg-12 field">\r\n' +
+    '<p><strong>Q1a. </strong>Quantos quilos aproximadamente?</p>\r\n' +
+    '<input type="text" name="Q1a" id="C23b"/>\r\n' +
+        '<hr>\r\n' +
+        '</div>\r\n';
+}
+
+function fragilityCalculator() {
+    
+
+}
